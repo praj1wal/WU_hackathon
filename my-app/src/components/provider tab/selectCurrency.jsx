8063bdedd9a5a5d61dced0d77902adf28ab8c333
prajwal kotalwar <prajwalkotalwar@gmail.com>
2020-11-10 18:33:15 +0530

    added forex rate

--- my-app/src/components/provider tab/selectCurrency.jsx
+++ my-app/src/components/provider tab/selectCurrency.jsx
@@ -3,6 +3,8 @@ import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import {FormControl,Select} from '@material-ui/core';
+import axios from 'axios';
+
 
 var codes = ["USD","INR","GBP","EUR","BHD","OMR","JOD","KYD","CHF","CAD"];
 function SelectCurrency() {
@@ -11,6 +13,8 @@ function SelectCurrency() {
   const [currSource,setCurr]= useState(null);
 
   const [currTarget,setTarget] = useState(null);
+  let [rate, setRate] = React.useState([]);
+
 
 
   const SourceClick = (event) => {
@@ -24,8 +28,23 @@ function SelectCurrency() {
   const handleClick = (event) =>{
 //    after button is clicked all forrex info should be displayed from backend
     setCurr(null);
-    setTarget(null);    
+    setTarget(null);
+    const fetchData = async() => {
+      axios({
+        "method": "GET",
+        "url": "https://api.exchangeratesapi.io/latest?base="+ currSource,
+      })
+      .then((response) => {
+        setRate(...rate,response.data.rates)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    };
+    fetchData();  
   };
+
+ 
 
   return (
     <div className="main_app">
@@ -67,6 +86,10 @@ function SelectCurrency() {
 
        </div>
        <Button id="button_div" onClick={handleClick}  variant="outlined" color="default">Submit</Button>
+       {/* {rate!==[] && rate.forEach((props)=>{
+         <h1>{props}</h1>
+       })} */}
+       {rate.CAD}
     </div>
   );
 }
