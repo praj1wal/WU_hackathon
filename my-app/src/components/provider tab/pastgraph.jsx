661600743c6d3bbb1e035d05ff1446c14ed91941
Sumit Shengokar <sumit.shengokar17@vit.edu>
2020-11-13 15:05:46 +0530

    added react-google-charts

--- my-app/src/components/provider tab/pastgraph.jsx
+++ my-app/src/components/provider tab/pastgraph.jsx
@@ -1,33 +1,42 @@
 import React,{useState} from 'react';
 import axios from 'axios';
 import {Button} from '@material-ui/core';
+
+import ReactDOM from "react-dom";
+import Chart from "react-google-charts";
 import {
     LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine,
   } from 'recharts';
 
 
   const data = [
-    {
-      name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-    },
-    {
-      name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-    },
-    {
-      name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-    },
-    {
-      name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-    },
-    {
-      name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-    },
-    {
-      name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-    },
-    {
-      name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-    },
+    [
+      {
+        type: "string",
+        id: "Date"
+      },
+      {
+        type: "number",
+        label: "Something"
+      },
+      {
+        type: "number",
+        label: "Something"
+      },
+      {
+        type: "number",
+        label: "Something"
+      },
+      {
+        type: "number",
+        label: "Something"
+      }
+    ],
+    ["Mon", 20, 28, 38, 45],
+    ["Tue", 31, 38, 55, 66],
+    ["Wed", 50, 55, 77, 80],
+    ["Thu", 77, 77, 66, 50],
+    ["Fri", 68, 66, 22, 15]
   ];
 
 
@@ -35,6 +44,7 @@ function Pastgraph({graphSource,graphTarget})
 {
 
    const [pastGraph, setPastGraph] = useState([]);
+
 
    const handleClick =(event) =>{
 
@@ -50,7 +60,7 @@ function Pastgraph({graphSource,graphTarget})
         // .catch((error) => {
         //   console.log(error)
         // })
-        const response = await axios(`https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2018-01-01&symbols=USD,INR`)
+        const response = await axios(`https://finnhub.io/api/v1/forex/candle?symbol=OANDA:EUR_USD&resolution=D&from=1572651390&to=1575243390&token=bun19kv48v6pkdmogb80`)
         console.log(response.data);
       };
       fetchData();
@@ -59,25 +69,12 @@ function Pastgraph({graphSource,graphTarget})
 
     return(
         <div>
-        <LineChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 20, right: 50, left: 20, bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <ReferenceLine x="Page C" stroke="red" label="Max PV PAGE" />
-        <ReferenceLine y={9800} label="Max" stroke="red" />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-      </LineChart>
-       
+         <Chart
+          chartType="CandlestickChart"
+          width="100%"
+          height="400px"
+          data={data}
+        />
 
        <Button onClick={handleClick}> click here </Button>
         </div>
