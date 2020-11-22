ba0e1536b060bd410142c9e838752991ade48607
prajwal kotalwar <prajwalkotalwar@gmail.com>
2020-11-22 14:20:16 +0530

    added flags

--- my-app/src/components/provider tab/ForexRate.jsx
+++ my-app/src/components/provider tab/ForexRate.jsx
@@ -4,6 +4,8 @@ import {Grid} from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
 import {makeStyles} from "@material-ui/core/styles";
+import ReactCountryFlag from "react-country-flag"
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -43,6 +45,7 @@ const ForexRate = () => {
                                     <Card className={classes.root} variant="outlined">
                                         <Typography gutterBottom variant="h5" component="h2">
                                             {key}
+                                            <ReactCountryFlag  countryCode={key.substring(0, 2)} svg />
                                         </Typography>
                                         <Typography gutterBottom variant="h6" component="h1">
                                             {rate[key]}
