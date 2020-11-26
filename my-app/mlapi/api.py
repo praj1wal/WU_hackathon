06a02d2e6bd1ce3ea5e9dd4ef1493c4e354e6ea4
DarshanAwachat <darshanawachat@gmail.com>
2020-11-26 16:54:42 +0530

    ml-222

--- my-app/mlapi/api.py
+++ my-app/mlapi/api.py
@@ -8,7 +8,13 @@ import datetime
 import time
 import math
 import requests
-model = keras.models.load_model('my_model') 
+import pandas as pd
+import io
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+import tensorflow
+
 app = Flask(__name__)
 CORS(app)
 
@@ -27,6 +33,7 @@ def hello_world():
 @app.route('/predict/', methods=['GET','POST'])
 def image_classifier():
     #r = requests.post('http://localhost:9000/v1/models/ImageClassifier:predict', json=payload)
+    model = keras.models.load_model('my_model') 
     datarec  = request.args.to_dict()
     src=datarec['IN']
     tar=datarec['OUT']
@@ -70,5 +77,56 @@ def image_classifier():
     #r = requests.post('http://localhost:9000/v1/models/ImageClassifier:predict', json=payload)
     x = model.predict(np.array([data]))
     return  jsonify(str(x[0][0]))
+
+@app.route('/predict2/', methods=['GET','POST'])
+def date_ml():
+    model = keras.models.load_model('my_model2')
+    datarec  = request.args.to_dict()
+    src=datarec['IN']
+    tar=datarec['OUT']
+    Current_Date = datetime.datetime.today()
+    #print("gmt:-", gmt) 
+    #Previous_Date = datetime.datetime.today() - datetime.timedelta(days=1200)
+    # ts stores timestamp 
+    no1 = str(math.ceil(time.mktime(Current_Date.timetuple())))
+    #no2 = str(math.ceil(time.mktime(Previous_Date.timetuple())))
+    download = requests.get("https://finnhub.io/api/v1/forex/candle?symbol=OANDA:"+ src +"_"+ tar +"&resolution=D&from=1480032000" + "&to=" + no1 + "&token=bun19kv48v6pkdmogb80&format=csv");
+    #download = requests.get('https://finnhub.io/api/v1/forex/candle?symbol=OANDA:EUR_USD&resolution=D&from=1480032000&to=1606262400&token=bun3n7748v6ubkqm42hg&format=csv')
+    df = pd.read_csv(io.StringIO(download.text))
+    # df['tt'] = pd.to_datetime(df['t']*1000, unit='ms').apply(lambda x: x.to_datetime64())
+    # df['Date'] = pd.to_datetime(df.tt,format='%Y-%m-%d')
+    # df['Date'] = pd.to_datetime(df['Date']).dt.date
+    data = df.sort_index(ascending=True, axis=0)
+    # print(data)
+    new_data = pd.DataFrame(index=range(0,len(df)),columns=[ 'c'])
+    i = 0
+    new_data.drop([ i for i in range(0,len(data)-60)], inplace = True)
     
+    for i in range(len(data) - 60,len(data)):
+        #new_data['Date'][i] = data['Date'][i]
+        new_data['c'][i] = data['c'][i]
+
+    print(new_data)
+    print(len(new_data))
+    #return jsonify(str(new_data[:61]))
+    # new_data.index = new_data.Date
+    # new_data.drop('Date', axis=1, inplace=True) 
+    scaler = MinMaxScaler(feature_range=(0, 1))
+
+    inputs = new_data.values
+    #return jsonify(str(inputs))
+    inputs = inputs.reshape(-1,1)
+    inputs  = scaler.fit_transform(inputs)
+
+    for i in range(60):
+        X_test = []
+        X_test.append(inputs)
+        X_test = np.array(X_test)
+        closing_price1 = model.predict(X_test)
+        inputs = np.append(np.delete(inputs, [0]),closing_price1)
+        inputs = inputs.reshape(-1,1)
+    inputs = scaler.inverse_transform(inputs)
+    inputa = inputs.tolist()
+    t = {"Array" : inputa}
+    return jsonify(t)
 
