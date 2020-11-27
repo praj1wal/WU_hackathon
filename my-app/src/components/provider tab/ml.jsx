d50db7e2f3d21f02eb653831f009a5fd26c279f5
prajwal kotalwar <prajwalkotalwar@gmail.com>
2020-11-27 13:10:02 +0530

    added prediction to ui

--- my-app/src/components/provider tab/ml.jsx
+++ my-app/src/components/provider tab/ml.jsx
@@ -32,14 +32,16 @@ export default function Prediction() {
 
  
     let [mlData, setMlData] = useState('');
+    let [predictData, setPredictData] = useState('');
+    let [diff, setDiff] = useState(1);
     let [mlState, setMlState] = useState(false);
-    const [selectedStartDate, setSelectedStartDate] = React.useState(new Date()-0);
+    const [selectedDate, setSelectedDate] = React.useState(new Date()-0);
     const currencies = useSelector(state => state.currencyreducer);
     const srcCurrency = currencies.srcCurrency;
     const tarCurrency = currencies.tarCurrency;
 
-    const handleStartDateChange = (date) => {
-        setSelectedStartDate(date-0);
+    const handleDateChange = (date) => {
+        setSelectedDate(date-0);
       };
 
 
@@ -54,19 +56,27 @@ export default function Prediction() {
         };
         fetchData()
     }
+    const handleClick1 = () => {
+
+        var difference = new Date()-0 - selectedDate;
+        var daysDifference = Math.floor(difference/1000/60/60/24);
+        setDiff(daysDifference);
+        const fetchData = async () => {
+            const response = await axios.get("http://127.0.0.1:5000/predict2/?IN=" + srcCurrency + "&OUT=" + tarCurrency);
+            setPredictData(response.data.Array[diff])
+            console.log("This is our great array", response.data.Array);
+        };
+        fetchData()
+    }
     return (
         <div >
             <Card style={{ height: '42vh' }}>
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
+            <Typography gutterBottom variant="h5" component="h2">
                         Prediction
             </Typography>
-                    {mlState === false && <Typography gutterBottom variant="h6" component="h2">
-                        Please Click On Predict Me To Get Prediction Of Next Day
-      </Typography>}
-                    <Typography>{mlState === true && mlData} {mlState === true && (mlData < 0.35 ? <h1>bearish</h1> : (mlData > 0.65 ? <h1>bullish</h1> : <h1>Cannot Determine</h1>))}</Typography>
-                </CardContent>
-
+            <Typography>
+                        Predict upto next 30 days
+      </Typography>
             <MuiPickersUtilsProvider utils={DateFnsUtils}>
             <form  noValidate>
                     <KeyboardDatePicker
@@ -76,18 +86,33 @@ export default function Prediction() {
                         margin="normal"
                         id="start-date-picker-inline"
                         label="Start Date picker inline"
-                        value={selectedStartDate}
-                        onChange={handleStartDateChange}
+                        value={selectedDate}
+                        onChange={handleDateChange}
                         KeyboardButtonProps={{
                             'aria-label': 'change date',
                         }}
                     />
                     
                     
-                    {/* <StyledButton id="button_div" onClick={OnClickHandler} variant="outlined" color="default">Submit</StyledButton> */}
+                    <StyledButton  onClick={handleClick1}>Submit</StyledButton>
                 </form>
-                </MuiPickersUtilsProvider>    
-
+                </MuiPickersUtilsProvider>  
+                {predictData!==undefined && <Card>
+                    <CardContent>
+                       <Typography>
+                            {console.log("This is pred",predictData)}
+                            {predictData}
+                            
+                        </Typography>
+                    </CardContent>
+                    </Card>}  
+                    <CardContent>
+                    
+                    {mlState === false && <Typography>
+                        Please Click On Predict Me To Get Prediction Of Next Day
+      </Typography>}
+                    <Typography>{mlState === true && mlData} {mlState === true && (mlData < 0.35 ? <h1>bearish</h1> : (mlData > 0.65 ? <h1>bullish</h1> : <h1>Cannot Determine</h1>))}</Typography>
+                </CardContent>
             </Card>
             
             <StyledButton onClick={handleClick}>Predict Me</StyledButton>
